736ce88e950f1c886efcdbd0aa2acf3a2fcd5b63
Abdoul_djabarou <babdouldjabar@yahoo.com>
2020-11-20 15:42:48 +0000

    implementation des utilite de firebase, avec la cpacite d'enregistrer et authentifier un utilisateur

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,35 @@ const config = {
     measurementId: "G-4VQB3H5QYY"
   };
 
+   export const createUserProfileDocument= async (userAuth,additionalData) => {
+       if(!userAuth) return;
+
+       //console.log(firestore.doc('users/128fdashadu'));
+       const userRef = firestore.doc(`users/${userAuth.uid}`);
+       const snapShot = await userRef.get();
+      // console.log(snapShot);
+       if(!snapShot.exists) {
+           const {displayName , email} = userAuth;
+           const createdAt = new Date();
+
+           try {
+               await userRef.set({
+                   displayName,
+                   email,
+                   createdAt,
+                   ...additionalData
+
+               })
+
+           } catch(error) {
+               console.log('error creating user',error.message);
+
+           }
+       }
+       return userRef;
+
+   };
+
   firebase.initializeApp(config);
 
   export const auth =firebase.auth();
