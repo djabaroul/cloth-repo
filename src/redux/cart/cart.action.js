bb9e2365cc841c3b2a1310655bee00a781fbb3ae
Abdoul_djabarou <babdouldjabar@yahoo.com>
2020-12-12 18:23:31 +0000

    deplacement de notre repertoire home et shop dtat dans son propre reduceur avec creation respective des selector et mise a jour correspondant au composant

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,5 +1,7 @@
 import CartActionTypes from './cart.types';
 
+
+
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
@@ -10,3 +12,14 @@ export const addItem = item =>({
     payload: item
 
 });
+export const clearItemFromCart = item => ({
+    type:CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
+
+export const removeItem = item => ({
+    type:CartActionTypes.REMOVE_ITEM,
+    payload:item
+}); 
+
+
